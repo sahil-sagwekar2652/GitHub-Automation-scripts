96a27082dda73d386794c7cd09ff27ffd8603093
Bhavya Prasad <125134136+bhavya1857@users.noreply.github.com>
2023-06-23 15:44:50 +0530

    Create github_secrets.py

---
+++ scripts/github_secrets.py
@@ -0,0 +1,17 @@
+import os
+
+def get_github_secrets():
+    """Get the GitHub personal access token and username from the environment."""
+    github_api_token = os.environ.get("GITHUB_API_TOKEN")
+    username = os.environ.get("USERNAME")
+    return github_api_token, username
+
+def main():
+    """Get the GitHub personal access token and username and print them to the console."""
+    github_api_token, username = get_github_secrets()
+    print("GitHub personal access token:", github_api_token)
+    print("GitHub username:", username)
+
+if __name__ == "__main__":
+    main()
+//this is in the reference to the code given in index.html where we see the github_secrets.py code is entirely missing. Its entirely missing from scripts folder too//
