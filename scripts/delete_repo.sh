49e8712b84b495e359cbe432a9269e4259124d5c
Bhavya Prasad <125134136+bhavya1857@users.noreply.github.com>
2023-07-29 19:00:04 +0530

    Update delete_repo.sh 1

--- scripts/delete_repo.sh
+++ scripts/delete_repo.sh
@@ -1,44 +1,26 @@
-#!/bin/bash
+import github_secrets
 
-# Prompt the user to enter the repository name
-read -p "Enter the name of the repository to delete: " repo_name
+def delete_repository(repo_name):
+    api_key = github_secrets.API_KEY
+    auth_header = "Authorization: token {}".format(api_key)
+    response = curl -X DELETE -s -H "{} " "https://api.github.com/repos/{}/".format(auth_header, repo_name)
 
-# Check if the repo name is empty
-if [[ -z $repo_name ]]; then
-  echo "Please enter a repository name."
-  exit 1
-fi
+    # Check the response status code
+    if response -ne 202:
+        # The response status code is not 202, which means the deletion failed.
+        # Check the response body for the error message.
+        error_message = echo response | jq -r '.message'
+        echo "An error occurred while deleting the repository."
+        echo "Response: $response"
+        echo "Error message: $error_message"
+        exit 1
 
-# Prompt for confirmation
-read -p "Are you sure you want to delete the repository '$repo_name'? This action cannot be undone. (y/n): " confirm
+    # The repository was deleted successfully.
+    echo "Repository '$repo_name' successfully deleted."
 
-# Convert the confirmation input to lowercase
-confirm=${confirm,,}
+if __name__ == "__main__":
+    repo_name = read -p "Enter the name of the repository to delete: "
+    delete_repository(repo_name)
 
-# Check if the user confirmed the deletion
-if [[ $confirm != "y" ]]; then
-  echo "Repository deletion canceled."
-  exit 0
-fi
-
-# Authenticate the user using personal access token (PAT)
-# Replace <PAT> with your actual personal access token
-auth_header="Authorization: token <PAT>"
-
-# Delete the repository using the GitHub API
-response=$(curl -X DELETE -s -H "$auth_header" "https://api.github.com/repos/$repo_name")
-
-# Check the response status code
-if [[ $response -ne 202 ]]; then
-  # The response status code is not 202, which means the deletion failed.
-  # Check the response body for the error message.
-  error_message=$(echo "$response" | jq -r '.message')
-  echo "An error occurred while deleting the repository."
-  echo "Error message: $error_message"
-  exit 1
-fi
-
-# The repository was deleted successfully.
-echo "Repository '$repo_name' successfully deleted."
 
 
