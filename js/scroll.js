d1dfb343e6bb95e86b65e58169cecc89708a2e30
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-08-06 22:48:40 +0530

    Back To Top Button 😎

---
+++ js/scroll.js
@@ -0,0 +1,20 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("scroll");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#0366d6 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
